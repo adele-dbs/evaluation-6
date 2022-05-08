db6ac82baa34d31ac664bc33b39b3bc2501f0fac
Adèle Dubois <adeledubois@orange.fr>
2022-05-08 14:00:28 +0200

    uppdate entityManager

--- entityManager.php
+++ entityManager.php
@@ -2,10 +2,20 @@
 
 function getEntityManager ()
 {
-      $dbParams = [
-        'driver' => 'pdo_mysql',
-        'user' => 'root',
-        'password' => '',
-        'dbname' => 'kgb',
-    ];
+    if (getenv('JAWSDB_URL') !== false) {
+        $dbparts = parse_url(getenv('JAWSDB_URL'));
+        $dbParams = [
+          'driver' => $dbparts['host'],
+          'user' => $dbparts['user'],
+          'password' => $dbparts['pass'],
+          'dbname' => ltrim($dbparts['path'],'/'),
+        ];
+    } else {
+        $dbParams = [
+          'driver' => 'pdo_mysql',
+          'user' => 'root',
+          'password' => '',
+          'dbname' => 'kgb',
+        ];
+    }  
 }
